8c7564c0214037e90f0bfb3496f998e3c0e6c7e9
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-05-04 20:53:15 +0700

    create base architecture

---
+++ src/components/pages/MainPage/index.tsx
@@ -0,0 +1,7 @@
+import {ROUTER_PATHS} from '@/constants';
+
+const MainPage = () => {
+  return <div>{ROUTER_PATHS.main}</div>;
+};
+
+export {MainPage};
---
+++ src/components/pages/Router/index.tsx
@@ -0,0 +1,5 @@
+const Router = () => {
+  return <div></div>;
+};
+
+export {Router};
