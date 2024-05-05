6c2ac0400e95045a451de5d3fa717438f854f8c8
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-05-05 22:44:41 +0700

    add form for character

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,3 +1,4 @@
 export * from './Header';
 export * from './DreamList';
 export * from './CharacterList';
+export * from './ChacterForm';
--- src/components/pages/CharacterCreatePage/index.tsx
+++ src/components/pages/CharacterCreatePage/index.tsx
@@ -1,9 +1,12 @@
 import { Container, PageTemplate } from '@/components/atoms';
+import { CharacterForm } from '@/components/organisms';
 
 const CharacterCreatePage = () => {
   return (
     <Container>
-      <PageTemplate title="Создание персонажа">a</PageTemplate>
+      <PageTemplate title="Создание персонажа">
+        <CharacterForm />
+      </PageTemplate>
     </Container>
   );
 };
