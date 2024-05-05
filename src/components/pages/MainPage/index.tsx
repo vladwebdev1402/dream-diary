1fa63445bd3bcfb6acfbc7d1ac3b39417263cc12
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-05-05 17:12:03 +0700

    add main page and adaptive

--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,9 +1,21 @@
-import { Container, DreamList } from '@/components';
+import { Button, Container, DreamList, Typography } from '@/components';
+
+import style from './style.module.scss';
 
 const MainPage = () => {
   return (
     <Container>
-      <DreamList />
+      <div className={style.head}>
+        <Typography variant="h2" component="h2">
+          Ваши сны
+        </Typography>
+        <Button variant="outlined" size="small">
+          Создать сон
+        </Button>
+      </div>
+      <div className={style.body}>
+        <DreamList />
+      </div>
     </Container>
   );
 };
