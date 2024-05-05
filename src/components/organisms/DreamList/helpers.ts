33645f037219e398dd30b8f1eec2d768c3475bd7
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-05-05 16:53:05 +0700

    add sort by date

---
+++ src/components/organisms/DreamList/helpers.ts
@@ -0,0 +1,20 @@
+import { calcTimestampDate } from '@/helpers';
+import { Dream } from '@/types';
+
+const groupByTimestamp = (dreams: Dream[]): Record<string, Dream[]> => {
+  const sortDreams = [...dreams].sort((a, b) => b.date.seconds - a.date.seconds);
+  const groupedDreams: Record<string, Dream[]> = {};
+
+  sortDreams.forEach((dream) => {
+    const date = calcTimestampDate(dream.date).toString();
+    if (groupedDreams[date] === undefined) {
+      groupedDreams[date] = [dream];
+      return;
+    }
+    groupedDreams[date].push(dream);
+  });
+
+  return groupedDreams;
+};
+
+export { groupByTimestamp };
